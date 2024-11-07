4036b41a61a0cf8d1844af5158773cb5b33f1534
Quantin <quantini31@hotmail.fr>
2024-11-07 10:04:38 +0100

    Added first keyboard combinations

--- inputs.py
+++ inputs.py
@@ -22,6 +22,7 @@ arduino = serial.Serial('/dev/ttyUSB0', 9600, timeout=0.01)
 arduino.reset_input_buffer()
 
 recentInputs = []
+inputsHistory = []
 
 def keyboard_input(display):
     global recentInputs
@@ -32,11 +33,35 @@ def keyboard_input(display):
         #Decode serial from arduino -> imput from keyboard
         line = arduino.readline().decode('utf-8').rstrip()
         if len(line) != 0:
-            if line == 'F':
-                display.stop()
-            elif line == 'D':
-                log.append("MASTER RESET")
-                cmd.append("conn_reset")
+            if line == 'A':   #Go
+                inputsHistory.append('Go')
+            elif line == 'B': #Reload
+                inputsHistory.append('Reload')
+            elif line == 'C': #Delete
+                inputsHistory.append('Delete')
+
+            elif line == 'E': #All
+                if inputsHistory[0] == 'Delete' or inputsHistory[0] == 'Reload':
+                    inputsHistory.append('All')
+            elif line == 'F': #Page
+                if inputsHistory[0] == 'Go' or inputsHistory[0] == 'Reload':
+                    inputsHistory.append('Page')
+
+            elif line == 'I': #Please
+                if inputsHistory[0] == 'Delete' and inputsHistory[1] == 'All':    #Delete all -> Alt F4
+                    display.stop()
+                elif inputsHistory[0] == 'Reload' and inputsHistory[1] == 'All':  #Reload all -> Reset connection
+                    log.append("MASTER RESET")
+                    cmd.append("conn_reset")
+                elif inputsHistory[0] == 'Reload' and inputsHistory[1] == 'Page': #Reload a page
+                    log.append("Feature not implemented yet")
+                elif inputsHistory[0] == 'Go' and inputsHistory[1] == 'Page': #Go Page x
+                    log.append("Feature not implemented yet")
+                inputsHistory.clear()
+      
+            elif line == 'H': #Clear
+                inputsHistory.clear()
+                
             #print(line)
             recentInputs.append(line)
 
